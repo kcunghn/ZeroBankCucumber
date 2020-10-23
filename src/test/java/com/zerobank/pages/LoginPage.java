49ea376329ced2fbbbb1bb9e6f84ea18d9e3d0ab
Erhan <erhanilgar@gmail.com>
2020-10-23 13:56:45 +0100

    new commit

--- src/test/java/com/zerobank/pages/LoginPage.java
+++ src/test/java/com/zerobank/pages/LoginPage.java
@@ -1,2 +1,28 @@
-package com.zerobank.pages;public class LoginPage {
+package com.zerobank.pages;
+
+import com.zerobank.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+    public LoginPage(){
+        PageFactory.initElements(Driver.get(), this);
+    }
+    @FindBy(id = "user_login")
+    public WebElement username;
+    @FindBy(id = "user_password")
+    public WebElement password;
+    @FindBy(css = "[name='submit']")
+    public WebElement submit;
+    @FindBy(id = "user_remember_me")
+    public WebElement keepMeSignedIn;
+    @FindBy(xpath = "//h3[contains(text(),'Log in to ZeroBank')]")
+    public WebElement loginPageTitle;
+    @FindBy(xpath = "//a[contains(text(),'Zero Bank')]")
+    public WebElement homePage;
+    @FindBy(xpath ="//a[contains(text(),'Forgot your password ?')]" )
+    public WebElement forgotPassword;
+
+
 }
---
+++ src/test/java/com/zerobank/runners/CukesRunner/CukesRunner.java
@@ -0,0 +1,17 @@
+package com.zerobank.runners.CukesRunner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"json:target/cucumber.json","html:target/default-html-reports"},
+        features = "src/test/resources/features",
+        glue = "com/zerobank/stepdefinitions",
+        dryRun = true,
+        tags = "@wip"
+)
+public class CukesRunner {
+}
